635f32bedf71b7aea38e0515a1eeaf8736bca2fb
Shubham <golubhandari405@gmail.com>
2026-07-09 05:07:42 +0000

    docs: setup intial project files

--- practice-url.js
+++
@@ -1,7 +0,0 @@
-const myUrl = new URL("https://example.com");
-
-myUrl.pathname = "products";
-myUrl.search = "?category=laptops";
-myUrl.searchParams.set("page", "2");
-myUrl.hash = "#reviews";
-console.log(myUrl.href);
